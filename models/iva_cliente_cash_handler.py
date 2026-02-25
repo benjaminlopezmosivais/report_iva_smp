fa69e0a1e546ff8affe2904d8487dfff2c2e6571
Benjamín López Monsiváis <lopez2373@hotmail.com>
2026-02-25 10:28:53 -0600

    feat: reporte IVA cliente cash con handler y columnas corregidas y reporte antes de validacion

--- models/iva_cliente_cash_handler.py
+++ models/iva_cliente_cash_handler.py
@@ -18,7 +18,7 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
         date_to = options.get("date", {}).get("date_to")
 
         domain = [
-            ("move_type", "=", "out_invoice"),
+            ("move_type", "in", ["out_invoice", "in_invoice","in_refund","out_refund"]),
             ("state", "=", "posted"),
             ("payment_state", "=", "paid"),
         ]
@@ -47,10 +47,11 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
 
             cols = [
                 {"name": mv.partner_id.name or ""},
+                {"name": mv.partner_id.vat or ""},
+                {"name": dict(mv._fields["move_type"].selection).get(mv.move_type)},  # 
                 {"name": mv.name or ""},
                 {"name": fields.Date.to_string(mv.invoice_date) if mv.invoice_date else ""},
                 {"name": fields.Date.to_string(payment_date) if payment_date else ""},
-
                 {"name": currency.round(vals["t_base_16"]), "class": "number"},
                 {"name": currency.round(vals["t_base_8"]), "class": "number"},
                 {"name": currency.round(vals["t_base_0"]), "class": "number"},
@@ -99,21 +100,29 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
     # ----------------------------------------------------------
 
     def _get_last_payment_date(self, mv):
-        """
-        Devuelve la ultima fecha de pago.
-        """
 
+        account_types = []
 
-        receivable_lines = mv.line_ids.filtered(
-            lambda l: l.account_id.account_type == "asset_receivable"
+        if mv.move_type in ("out_invoice", "out_refund"):
+            account_types = ["asset_receivable"]
+
+        elif mv.move_type in ("in_invoice", "in_refund"):
+            account_types = ["liability_payable"]
+
+        lines = mv.line_ids.filtered(
+            lambda l: l.account_id.account_type in account_types
         )
 
         dates = []
 
-        for line in receivable_lines:
+        for line in lines:
             for m in line.matched_credit_ids:
                 if m.credit_move_id and m.credit_move_id.move_id:
                     dates.append(m.credit_move_id.move_id.date)
+
+            for m in line.matched_debit_ids:
+                if m.debit_move_id and m.debit_move_id.move_id:
+                    dates.append(m.debit_move_id.move_id.date)
 
         return max(dates) if dates else False
 
@@ -187,7 +196,7 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
 
         res["a_base_8"], res["a_iva_8"] = self._get_tax_base_and_amount(
             mv,
-            tax_type="sale",
+            tax_type="purchase",
             tax_rate=8
         )
 
@@ -197,32 +206,50 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
             tax_rate=0
         )
 
-        res["a_base_exenta"], res["a_iva_exento"] = self._get_tax_base_and_amount(
+        res["a_no_deducible"] = self._get_tax_amount_by_external_id(
             mv,
-            tax_type="purchase"
+            "iva_cliente_cash_report.tax_no_deducible_16"
         )
+        
+        res["a_base_exenta"] = self._get_tax_amount_by_external_id(
+            mv,
+            "iva_cliente_cash_report.tax_a_base_exenta"
+        )
+        
+        res["a_no_objeto"] = self._get_tax_amount_by_external_id(
+            mv,
+            "iva_cliente_cash_report.tax_a_no_objeto"
+        )
+
+        res["_iva_16_imp"]= self._get_tax_amount_for_account(mv, "118.02.01")
+
         # ----------------------------------
         # RETENCIONES (un solo loop)
         # ----------------------------------
 
         for tax_line in mv.line_ids.filtered(lambda l: l.tax_line_id):
-
             tax = tax_line.tax_line_id
             amount = abs(tax_line.balance)
 
             if tax.type_tax_use == "sale":
+                activity = mv.partner_id.activity_type
 
-                if tax.amount == -4:
-                    res["riva_fletes_4"] += amount
+                if activity == "fletes":
+                    res["riva_fletes_4"] += amount * .04
 
-                elif abs(tax.amount - -10.67) < 0.01:
-                    res["riva_servprof_1067"] += amount
+                elif activity == "servicios":
+                    res["riva_servprof_1067"] += amount * .1067
 
-                elif tax.amount == -10:
-                    res["risr_servprof_10"] += amount
+                elif activity == "arrendamiento":
+                    res["riva_arr_1067"] += amount * .1067
+                    res["risr_arr_10"] += amount* .10
 
-                elif tax.amount == -1.25:
-                    res["risr_resico_125"] += amount
+                elif activity == "actividad_empresarial":
+                    res["risr_servprof_10"] += amount * .10
+
+                elif activity == "otros":
+                    res["risr_resico_125"] += amount * .0125
+
 
         # ----------------------------------
         # TOTALES MULTIMONEDA
@@ -234,14 +261,14 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
 
         return res
 
-    def _get_tax_base_and_amount(self, mv, tax_type="sale", tax_rate=None):
+    def _get_tax_base_and_amount(self, mv, tax_type="sale" , tax_rate=None):
 
         base = 0.0
         amount = 0.0
 
         # BASE
         for line in mv.line_ids:
-            if line.account_id.account_type == "income":
+            if line.account_id.account_type in ("income", "expense"):
 
                 for tax in line.tax_ids:
                     if tax.type_tax_use == tax_type:
@@ -260,3 +287,52 @@ class ReportIvaClienteCashHandler(models.AbstractModel):
                     amount += abs(line.balance)
 
         return base, amount
+
+    def _get_tax_by_external_id(self, mv, tax_external_id):
+
+        tax = self.env.ref(tax_external_id, raise_if_not_found=False)
+
+        if not tax:
+            return 0.0, 0.0
+
+        base = 0.0
+        amount = 0.0
+
+        for line in mv.line_ids:
+            if tax in line.tax_ids:
+                base += abs(line.balance)
+
+        for line in mv.line_ids.filtered(lambda l: l.tax_line_id == tax):
+            amount += abs(line.balance)
+
+        return base, amount
+
+
+
+    def _get_tax_amount_by_external_id(self, mv, tax_external_id):
+
+        tax = self.env.ref(tax_external_id, raise_if_not_found=False)
+
+        if not tax:
+            return 0.0
+
+        amount = 0.0
+        for line in mv.line_ids:
+            if tax in line.tax_ids:
+                amount += abs(line.balance)
+
+        for line in mv.line_ids.filtered(lambda l: l.tax_line_id == tax):
+            amount += abs(line.balance)
+
+        return amount
+
+
+
+    ##Importacion
+    def _get_tax_amount_for_account(self, mv, account_code):
+        target_account = self.env['account.account'].search([('code', '=', account_code)], limit=1)
+        amount = 0.0
+        if target_account:
+            for line in mv.line_ids.filtered(lambda l: l.tax_line_id and l.account_id == target_account):
+                amount += abs(line.balance)
+        return amount
